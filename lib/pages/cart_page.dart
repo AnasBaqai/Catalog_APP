b107f0bf5ec4fb8659c1653b20cf20a2be75f440
anas <anashabib139@gmail.com>
2021-08-10 16:24:05 +0500

    day19 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,6 +12,56 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTool(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTool extends StatelessWidget {
+  const _CartTool({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(context.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(context.theme.buttonColor),
+                  ),
+                  child: "buy".text.white.make())
+              .w16(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatelessWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+            onPressed: () {}, icon: Icon(Icons.remove_circle_outline)),
+        title: "item 1".text.make(),
+      ),
     );
   }
 }
